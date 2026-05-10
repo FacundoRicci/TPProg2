d242b849dee33771de62cf14b04af766e5631d7f
Lautaro2811 <lau.alvarado.2811@gmail.com>
2026-05-10 20:11:27 -0300

    Creacion de la pagina Catalogo
    Me habia olvidado de hacer el commit. pero basicamente hice la estructura del html, cree los estilos css, cree el archivo catalogo.js para hacer la logica correspondiente y modifique el archivo JSON porque habia problemas con las rutas de las imagenes

---
+++ assets/js/catalogo.js
@@ -0,0 +1,19 @@
+fetch("/db.json")
+    .then(response => response.json())
+    .then(datos => {
+        const catalogo = document.getElementById("catalogo");
+
+        datos.productos.forEach(libro => {
+            const libros = datos.productos;
+
+            catalogo.innerHTML += `
+                <div class="producto">
+                    <img class="imagen-producto" src="${libro.image}">
+                    <p class="titulo-producto">${libro.name}</p>
+                    <p class="autor-producto">${libro.author}</p>
+                    <p class="precio">${libro.precio}</p>
+                    <button class="btn-agregar" id="agregarCarrito">Agregar al carrito</button>
+                </div>
+            `;
+        });
+    })
